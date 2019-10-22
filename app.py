67b2074edb4e833d47cb4d482af45b2fa7d57717
unkind58 <leo.lapenas88@gmail.com>
2019-10-22 22:56:28 +0300

    Add Roman to Arabic and reverse option

--- app.py
+++ app.py
@@ -28,5 +28,48 @@ def choose():
             continue                        #should be quit()
 
 
-print(choose())
-print(intro())
+#print(choose())
+#print(intro())
+
+roman_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+arabic_to_romans = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400,
+                    "D": 500, "CM": 900, "M": 1000}
+
+
+class converter:
+    def rta(self, number):
+        arabic = []
+        for j in number:
+            if number.count("D") > 1 or number.count("L") > 1 or number.count("I") > 3 or number.count("V") > 1\
+                    or number.count("IV") > 1 or number.count("IX") > 1 or number.count("X") > 3\
+                    or number.count("XL") > 1 or number.count("XC") > 1 or number.count("CD") > 1\
+                    or number.count("C") > 3 or number.count("M") > 3:
+                return converter().rta(str(input("You entered incorrect Roman number."
+                                                 " Please enter again. roman to arabic: ").upper()))
+        for i in range(len(number)):
+            if i > 0 and roman_to_arabic[number[i]] > roman_to_arabic[number[i - 1]]:
+                arabic.append(roman_to_arabic[number[i]] - 2 * roman_to_arabic[number[i - 1]])
+            else:
+                arabic.append(roman_to_arabic[number[i]])
+        return sum(arabic)
+
+
+#print(converter().rta(str(input("roman to arabic: ").upper())))
+
+
+def atr(number):
+    range_flag = None
+    if number < 0:
+        return  "Please enter positive number ahd higher than 0."
+    for key, value in arabic_to_romans.items():
+        if value == number:
+            return key
+        if number > value:
+            range_flag = key
+    remaining = number - arabic_to_romans[range_flag]
+    #print(remaining)
+    #print(number)
+    return range_flag + atr(remaining)
+
+
+#print(atr(int(input("arabic to roman: "))))
