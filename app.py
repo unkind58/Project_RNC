82dc99e5a45e2403e81aac987d691a53dac5ec6e
unkind58 <leo.lapenas88@gmail.com>
2019-10-28 19:37:00 +0200

    Update solution for at()  function

--- app.py
+++ app.py
@@ -31,8 +31,6 @@ def choose():
 
 
 roman_to_arabic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-arabic_to_romans = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, "XC": 90, "C": 100, "CD": 400,
-                    "D": 500, "CM": 900, "M": 1000}
 
 
 class Converter:
@@ -59,14 +57,17 @@ def atr(number):
     """ conversion from Arabic to Roman"""
     if number <= 0:
         return atr(int(input("Please enter positive number ahd higher than 0. Arabic to Roman: ")))
-    range_flag = None
-    for key, value in arabic_to_romans.items():
-        if value == number:
-            return key
-        if number > value:
-            range_flag = key
-    remaining = number - arabic_to_romans[range_flag]
-    return range_flag + atr(remaining)
+    arabic_num = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+    romans_num = ('M',  'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+    atr_list = list()
+    number_atr = number
+    for i in range(len(arabic_num)):
+        counter = (number // arabic_num[i])
+        atr_list.append(romans_num[i] * counter)
+        number = number - arabic_num[i] * counter
+    atr_result = "".join(atr_list)
+    print("Arabic", number_atr, "in Roman numeral system is:", atr_result)
+    return reply()
 
 
 def reply():
